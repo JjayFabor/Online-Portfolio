a9cc2e3130ea52270655e975babff03c3c6f41b5
JjayFabor <faborjaylordvhan@gmail.com>
2024-07-28 16:40:03 +0800

    ADD: Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import About from './components/About';
+import Experience from './components/Experience';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import ParticlesComponent from './components/Particles';
@@ -19,14 +20,14 @@ const App = () => {
         <div id="home" className="h-screen flex items-center justify-center relative mb-144">
           <Home />
         </div>
-        <div id="about" className="h-screen flex items-center justify-center relative">
+        <div id="about" className="h-screen flex items-center justify-center relative mb-166">
           <About />
         </div>
-        <div id="projects" className="h-screen flex items-center justify-center relative">
+        <div id="projects" className="h-screen flex items-center justify-center relative mb-166">
           <Projects/>
         </div>
-        <div id="resume" className="h-screen flex items-center justify-center relative">
-          <h1 className="text-4xl">Resume Section</h1>
+        <div id="experience" className="h-screen flex items-center justify-center relative">
+          <Experience/>
         </div>
         <div id="contact" className="h-screen flex items-center justify-center relative">
           <h1 className="text-4xl">Contact Section</h1>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
-const Resume = () => {
+const Experience = () => {
   return (
-    <div className="h-screen flex items-center justify-center relative">
-      <h1 className="text-4xl">Resume Section</h1>
-    </div>
+      <div className='flex justify-center items-center mt-52 font-dinasti-bold text-9xl'>
+        <TypeAnimation
+          sequence={[
+            'NO EXPERIENCE🥲',
+            1000,          
+            '',              
+            1000              
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ display: 'inline-block' }}
+          repeat={Infinity}
+        />
+      </div>
   );
-};
+}
 
-export default Resume;
+export default Experience;
